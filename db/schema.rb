4e860685fa0047f262b06549fd5ecb8541c4aecb
Babyyodabc <rjwahba@gmail.com>
2020-04-24 15:39:07 -0400

    change coords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_182632) do
+ActiveRecord::Schema.define(version: 2020_04_24_193255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_182632) do
     t.integer "bath"
     t.integer "sqr_foot"
     t.string "default_image"
-    t.float "longitude"
-    t.float "latitude"
+    t.decimal "longitude", precision: 10, scale: 6
+    t.decimal "latitude", precision: 10, scale: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
