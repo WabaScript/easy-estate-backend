db88f5df0ef31a688cc750ca2df12345e52c792c
Babyyodabc <rjwahba@gmail.com>
2020-04-28 14:21:26 -0400

    updated new user render

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,12 +16,12 @@ class Api::V1::UsersController < ApplicationController
       
         # POST /users
         def create
-          @user = User.new(user_params)
+          user = User.new(user_params)
       
-          if @user.save
-            render json: @user, status: :created, location: @user
+          if user.save
+            render json: user, status: :created
           else
-            render json: @user.errors, status: :unprocessable_entity
+            render json: user.errors, status: :unprocessable_entity
           end
         end
       
